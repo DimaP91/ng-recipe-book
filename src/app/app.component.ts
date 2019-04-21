c6605a8196bedc3c04dc40a916d79e71262d1798
Dmytro Prokopiuk <dmytroprokopiuk@MacBook-Pro-Dmytro.local>
2019-04-21 09:49:58 +0300

    test pritter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase/app';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+	selector: 'app-root',
+	templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
-
-  ngOnInit() {
-    firebase.initializeApp({
-      apiKey: 'AIzaSyByYH8frbng4HhsuKq35Mm2OYcFnzrn4ak',
-      authDomain: 'ng-recipe-book-e1671.firebaseapp.com',
-    });
-  }
+export class AppComponent {
+	ngOnInit() {
+		firebase.initializeApp({
+			apiKey: 'AIzaSyByYH8frbng4HhsuKq35Mm2OYcFnzrn4ak',
+			authDomain: 'ng-recipe-book-e1671.firebaseapp.com'
+		});
+	}
 }
